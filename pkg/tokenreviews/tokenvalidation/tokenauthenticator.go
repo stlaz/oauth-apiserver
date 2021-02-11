4e212f9f286a6abeeda1ea087d5138eb22a1c7cf
Stanislav Laznicka <slaznick@redhat.com>
2021-02-11 12:16:18 +0100

    change tokenAuthenticator's interface to match authenticator.Request
    This also merges bootstrapAuthenticator (kube:admin) and
    tokenAuthenticator together.
    
    The old code kube-apiserver code required the tokenAuthenticator to
    adhere to the authenticator.Token interface, whereas the upstream
    TokenReview's REST storage expects the authenticator.Request interface.
    
    During the interface swap, the logic of bootstrap (kube:admin) and
    the openshift authenticators was merged into one token authenticator
    which performs further validation based on the username. The duality
    of these authenticators has caused us headaches in the past, but
    that should now be solved.
    
    Also unit tests for the reworked authenticator were added, resupplying
    the test cases that got missing from previous moves and adding a couple
    of new ones.

--- pkg/tokenreviews/tokenvalidation/tokenauthenticator.go
+++ pkg/tokenreviews/tokenvalidation/tokenauthenticator.go
@@ -1,84 +1,191 @@
-package oauth
+package tokenvalidation
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
 	"errors"
 	"fmt"
+	"net/http"
+	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	kauthenticator "k8s.io/apiserver/pkg/authentication/authenticator"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apiserver/pkg/authentication/authenticator"
+	"k8s.io/apiserver/pkg/authentication/user"
 	kuser "k8s.io/apiserver/pkg/authentication/user"
 
 	authorizationv1 "github.com/openshift/api/authorization/v1"
-	oauthclient "github.com/openshift/client-go/oauth/clientset/versioned/typed/oauth/v1"
-	userclient "github.com/openshift/client-go/user/clientset/versioned/typed/user/v1"
+	oauthv1 "github.com/openshift/api/oauth/v1"
+	userv1 "github.com/openshift/api/user/v1"
+	oauthv1client "github.com/openshift/client-go/oauth/clientset/versioned/typed/oauth/v1"
+	userv1client "github.com/openshift/client-go/user/clientset/versioned/typed/user/v1"
+	bootstrap "github.com/openshift/library-go/pkg/authentication/bootstrapauthenticator"
+
+	tokenvalidators "github.com/openshift/oauth-apiserver/pkg/tokenreviews/tokenvalidation/validators"
+)
+
+const (
+	clusterAdminGroup       = "system:cluster-admins"
+	authenticatedOAuthGroup = "system:authenticated:oauth"
+
+	sha256Prefix = "sha256~"
 )
 
 var errLookup = errors.New("token lookup failed")
 
-type tokenAuthenticator struct {
-	tokens       oauthclient.OAuthAccessTokenInterface
-	users        userclient.UserInterface
-	groupMapper  UserToGroupMapper
-	validators   OAuthTokenValidator
-	implicitAuds kauthenticator.Audiences
+var _ authenticator.Request = &TokenAuthenticator{}
+
+type TokenAuthenticator struct {
+	accessTokenClient   oauthv1client.OAuthAccessTokenInterface
+	userClient          userv1client.UserInterface
+	bootstrapUserGetter bootstrap.BootstrapUserDataGetter
+	groupMapper         UserToGroupMapper
+
+	validators tokenvalidators.OAuthTokenValidator
 }
 
-func NewTokenAuthenticator(tokens oauthclient.OAuthAccessTokenInterface, users userclient.UserInterface, groupMapper UserToGroupMapper, implicitAuds kauthenticator.Audiences, validators ...OAuthTokenValidator) kauthenticator.Token {
-	return &tokenAuthenticator{
-		tokens:       tokens,
-		users:        users,
-		groupMapper:  groupMapper,
-		validators:   OAuthTokenValidators(validators),
-		implicitAuds: implicitAuds,
+func NewTokenAuthenticator(
+	accessTokenClient oauthv1client.OAuthAccessTokenInterface,
+	bootstrapUserGetter bootstrap.BootstrapUserDataGetter,
+	userClient userv1client.UserInterface,
+	groupMapper UserToGroupMapper,
+	validators ...tokenvalidators.OAuthTokenValidator,
+) *TokenAuthenticator {
+	return &TokenAuthenticator{
+		accessTokenClient:   accessTokenClient,
+		userClient:          userClient,
+		bootstrapUserGetter: bootstrapUserGetter,
+		groupMapper:         groupMapper,
+
+		validators: tokenvalidators.OAuthTokenValidators(validators),
 	}
 }
 
-func (a *tokenAuthenticator) AuthenticateToken(ctx context.Context, name string) (*kauthenticator.Response, bool, error) {
-	token, err := a.tokens.Get(context.TODO(), name, metav1.GetOptions{})
-	if err != nil {
-		return nil, false, errLookup // mask the error so we do not leak token data in logs
+func (h *TokenAuthenticator) AuthenticateRequest(req *http.Request) (*authenticator.Response, bool, error) {
+	authorizationHeader := strings.TrimSpace(req.Header.Get("Authorization"))
+	if len(authorizationHeader) == 0 {
+		return nil, false, nil
 	}
 
-	user, err := a.users.Get(context.TODO(), token.UserName, metav1.GetOptions{})
+	authzHeaderSplit := strings.Split(authorizationHeader, " ")
+	if len(authzHeaderSplit) < 2 || strings.ToLower(authzHeaderSplit[0]) != "bearer" {
+		return nil, false, nil
+	}
+
+	tokenString := authzHeaderSplit[1]
+	if len(tokenString) == 0 {
+		return nil, false, nil
+	}
+
+	// normalize to sha256 form if sha256 token
+	if strings.HasPrefix(tokenString, sha256Prefix) {
+		h := sha256.Sum256([]byte(strings.TrimPrefix(tokenString, sha256Prefix)))
+		tokenString = sha256Prefix + base64.RawURLEncoding.EncodeToString(h[:])
+	}
+
+	userInfo, err := h.gatherUserInfo(context.TODO(), tokenString)
 	if err != nil {
 		return nil, false, err
 	}
 
-	if err := a.validators.Validate(token, user); err != nil {
-		return nil, false, err
+	reqAuds, _ := authenticator.AudiencesFrom(req.Context())
+
+	return &authenticator.Response{
+		User:      userInfo,
+		Audiences: reqAuds, // OpenShift does not deal with audiences in its access tokens, send the audiences that were expected
+	}, true, nil
+}
+
+func (h *TokenAuthenticator) gatherUserInfo(ctx context.Context, tokenName string) (user.Info, error) {
+	token, err := h.accessTokenClient.Get(ctx, tokenName, metav1.GetOptions{})
+	if err != nil {
+		return nil, errLookup // mask the error so we do not leak token data in logs
 	}
 
-	groups, err := a.groupMapper.GroupsFor(user.Name)
+	var user *userv1.User
+	var groupNames []string
+	// the bootstrap user is special-cased in authentication
+	if token.UserName == bootstrap.BootstrapUser {
+		user, groupNames, err = h.getBootstrapUser(token)
+	} else {
+		user, groupNames, err = h.getOpenShiftUser(ctx, token)
+	}
+
 	if err != nil {
-		return nil, false, err
+		return nil, err
+	}
+	if user == nil {
+		return nil, fmt.Errorf("no such user")
+	}
+
+	return &kuser.DefaultInfo{
+		Name:   user.Name,
+		UID:    string(user.UID),
+		Groups: groupNames,
+		Extra: map[string][]string{
+			authorizationv1.ScopesKey: token.Scopes,
+		},
+	}, nil
+}
+
+func (h *TokenAuthenticator) getBootstrapUser(token *oauthv1.OAuthAccessToken) (*userv1.User, []string, error) {
+	data, ok, err := h.bootstrapUserGetter.Get()
+	if err != nil || !ok {
+		return nil, nil, err
+	}
+
+	// this allows us to reuse existing validators
+	// since the uid is based on the secret, if the secret changes, all
+	// tokens issued for the bootstrap user before that change stop working
+	fakeUser := &userv1.User{
+		ObjectMeta: metav1.ObjectMeta{
+			UID:  types.UID(data.UID),
+			Name: "kube:admin",
+		},
+	}
+
+	if err := h.validators.Validate(token, fakeUser); err != nil {
+		return nil, nil, err
+	}
+
+	// we cannot use SystemPrivilegedGroup because it cannot be properly scoped.
+	// see openshift/origin#18922 and how loopback connections are handled upstream via AuthorizeClientBearerToken.
+	// api aggregation with delegated authorization makes this impossible to control, see WithAlwaysAllowGroups.
+	// an openshift specific cluster role binding binds ClusterAdminGroup to the cluster role cluster-admin.
+	// thus this group is authorized to do everything via RBAC.
+	// this does make the bootstrap user susceptible to anything that causes the RBAC authorizer to fail.
+	// this is a safe trade-off because scopes must always be evaluated before RBAC for them to work at all.
+	// a failure in that logic means scopes are broken instead of a specific failure related to the bootstrap user.
+	// if this becomes a problem in the future, we could generate a custom extra value based on the secret content
+	// and store it in BootstrapUserData, similar to how UID is calculated.  this extra value would then be wired
+	// to a custom authorizer that allows all actions.  the problem with such an approach is that since we do not
+	// allow remote authorizers in OpenShift, the BootstrapUserDataGetter logic would have to be shared between the
+	// the kube api server and osin instead of being an implementation detail hidden inside of osin.  currently the
+	// only shared code is the value of the BootstrapUser constant (since it is special cased in validation)
+	return fakeUser, []string{clusterAdminGroup}, nil
+}
+
+func (h *TokenAuthenticator) getOpenShiftUser(ctx context.Context, token *oauthv1.OAuthAccessToken) (*userv1.User, []string, error) {
+	user, err := h.userClient.Get(ctx, token.UserName, metav1.GetOptions{})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if err := h.validators.Validate(token, user); err != nil {
+		return nil, nil, err
+	}
+
+	groups, err := h.groupMapper.GroupsFor(user.Name)
+	if err != nil {
+		return nil, nil, err
 	}
 	groupNames := make([]string, 0, len(groups))
 	for _, group := range groups {
 		groupNames = append(groupNames, group.Name)
 	}
 
-	tokenAudiences := a.implicitAuds
-	requestedAudiences, ok := kauthenticator.AudiencesFrom(ctx)
-	if !ok {
-		// default to apiserver audiences
-		requestedAudiences = a.implicitAuds
-	}
-
-	auds := kauthenticator.Audiences(tokenAudiences).Intersect(requestedAudiences)
-	if len(auds) == 0 && len(a.implicitAuds) != 0 {
-		return nil, false, fmt.Errorf("token audiences %q is invalid for the target audiences %q", tokenAudiences, requestedAudiences)
-	}
-
-	return &kauthenticator.Response{
-		User: &kuser.DefaultInfo{
-			Name:   user.Name,
-			UID:    string(user.UID),
-			Groups: groupNames,
-			Extra: map[string][]string{
-				authorizationv1.ScopesKey: token.Scopes,
-			},
-		},
-		Audiences: auds,
-	}, true, nil
+	// append system:authenticated:oauth group because if you have an OAuth
+	// bearer token, you're a human (usually)
+	return user, append(groupNames, authenticatedOAuthGroup), nil
 }
--- pkg/tokenreviews/tokenvalidation/tokenauthenticator_test.go
+++ pkg/tokenreviews/tokenvalidation/tokenauthenticator_test.go
@@ -1,23 +1,32 @@
-package oauth
+package tokenvalidation
 
 import (
 	"context"
 	"errors"
+	"fmt"
+	"net/http/httptest"
 	"testing"
 	"time"
 
+	"k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/clock"
+	"k8s.io/apimachinery/pkg/util/diff"
 	"k8s.io/apiserver/pkg/authentication/authenticator"
+	"k8s.io/apiserver/pkg/authentication/user"
+	corefake "k8s.io/client-go/kubernetes/fake"
 	clienttesting "k8s.io/client-go/testing"
+	"k8s.io/client-go/tools/cache"
 
 	oauthv1 "github.com/openshift/api/oauth/v1"
 	userv1 "github.com/openshift/api/user/v1"
 	oauthfake "github.com/openshift/client-go/oauth/clientset/versioned/fake"
-	oauthclient "github.com/openshift/client-go/oauth/clientset/versioned/typed/oauth/v1"
 	userfake "github.com/openshift/client-go/user/clientset/versioned/fake"
+	userinformer "github.com/openshift/client-go/user/informers/externalversions"
+	bootstrap "github.com/openshift/library-go/pkg/authentication/bootstrapauthenticator"
+
+	"github.com/openshift/oauth-apiserver/pkg/tokenreviews/tokenvalidation/usercache"
+	tokenvalidators "github.com/openshift/oauth-apiserver/pkg/tokenreviews/tokenvalidation/validators"
 )
 
 func TestAuthenticateTokenInvalidUID(t *testing.T) {
@@ -29,14 +38,14 @@ func TestAuthenticateTokenInvalidUID(t *testing.T) {
 			UserUID:    string("bar1"),
 		},
 	)
+	fakeKubeClient := corefake.NewSimpleClientset()
 	fakeUserClient := userfake.NewSimpleClientset(&userv1.User{ObjectMeta: metav1.ObjectMeta{Name: "foo", UID: "bar2"}})
 
-	tokenAuthenticator := NewTokenAuthenticator(fakeOAuthClient.OauthV1().OAuthAccessTokens(), fakeUserClient.UserV1().Users(), NoopGroupMapper{}, nil, NewUIDValidator())
+	bootstrapDataGetter := bootstrap.NewBootstrapUserDataGetter(fakeKubeClient.CoreV1(), fakeKubeClient.CoreV1())
 
-	userInfo, found, err := tokenAuthenticator.AuthenticateToken(context.TODO(), "token")
-	if found {
-		t.Error("Found token, but it should be missing!")
-	}
+	tokenAuthenticator := NewTokenAuthenticator(fakeOAuthClient.OauthV1().OAuthAccessTokens(), bootstrapDataGetter, fakeUserClient.UserV1().Users(), NoopGroupMapper{}, tokenvalidators.NewUIDValidator())
+
+	userInfo, err := tokenAuthenticator.gatherUserInfo(context.TODO(), "token")
 	if err.Error() != "user.UID (bar2) does not match token.userUID (bar1)" {
 		t.Errorf("Unexpected error: %v", err)
 	}
@@ -48,12 +57,13 @@ func TestAuthenticateTokenInvalidUID(t *testing.T) {
 func TestAuthenticateTokenNotFoundSuppressed(t *testing.T) {
 	fakeOAuthClient := oauthfake.NewSimpleClientset()
 	fakeUserClient := userfake.NewSimpleClientset()
-	tokenAuthenticator := NewTokenAuthenticator(fakeOAuthClient.OauthV1().OAuthAccessTokens(), fakeUserClient.UserV1().Users(), NoopGroupMapper{}, nil)
+	fakeKubeClient := corefake.NewSimpleClientset()
 
-	userInfo, found, err := tokenAuthenticator.AuthenticateToken(context.TODO(), "token")
-	if found {
-		t.Error("Found token, but it should be missing!")
-	}
+	bootstrapDataGetter := bootstrap.NewBootstrapUserDataGetter(fakeKubeClient.CoreV1(), fakeKubeClient.CoreV1())
+
+	tokenAuthenticator := NewTokenAuthenticator(fakeOAuthClient.OauthV1().OAuthAccessTokens(), bootstrapDataGetter, fakeUserClient.UserV1().Users(), NoopGroupMapper{})
+
+	userInfo, err := tokenAuthenticator.gatherUserInfo(context.TODO(), "token")
 	if err != errLookup {
 		t.Error("Expected not found error to be suppressed with lookup error")
 	}
@@ -64,16 +74,16 @@ func TestAuthenticateTokenNotFoundSuppressed(t *testing.T) {
 
 func TestAuthenticateTokenOtherGetErrorSuppressed(t *testing.T) {
 	fakeOAuthClient := oauthfake.NewSimpleClientset()
+	fakeKubeClient := corefake.NewSimpleClientset()
+	bootstrapDataGetter := bootstrap.NewBootstrapUserDataGetter(fakeKubeClient.CoreV1(), fakeKubeClient.CoreV1())
+
 	fakeOAuthClient.PrependReactor("get", "oauthaccesstokens", func(action clienttesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, nil, errors.New("get error")
 	})
 	fakeUserClient := userfake.NewSimpleClientset()
-	tokenAuthenticator := NewTokenAuthenticator(fakeOAuthClient.OauthV1().OAuthAccessTokens(), fakeUserClient.UserV1().Users(), NoopGroupMapper{}, nil)
+	tokenAuthenticator := NewTokenAuthenticator(fakeOAuthClient.OauthV1().OAuthAccessTokens(), bootstrapDataGetter, fakeUserClient.UserV1().Users(), NoopGroupMapper{})
 
-	userInfo, found, err := tokenAuthenticator.AuthenticateToken(context.TODO(), "token")
-	if found {
-		t.Error("Found token, but it should be missing!")
-	}
+	userInfo, err := tokenAuthenticator.gatherUserInfo(context.TODO(), "token")
 	if err != errLookup {
 		t.Error("Expected custom get error to be suppressed with lookup error")
 	}
@@ -82,41 +92,190 @@ func TestAuthenticateTokenOtherGetErrorSuppressed(t *testing.T) {
 	}
 }
 
-
-
-
-
-
-
-func checkToken(t *testing.T, name string, authf authenticator.Token, tokens oauthclient.OAuthAccessTokenInterface, current clock.Clock, present bool) {
-	t.Helper()
-	userInfo, found, err := authf.AuthenticateToken(context.TODO(), name)
-	if present {
-		if !found {
-			t.Errorf("Did not find token %s!", name)
-		}
-		if err != nil {
-			t.Errorf("Unexpected error checking for token %s: %v", name, err)
-		}
-		if userInfo == nil {
-			t.Errorf("Did not get a user for token %s!", name)
-		}
-	} else {
-		if found {
-			token, tokenErr := tokens.Get(context.TODO(), name, metav1.GetOptions{})
-			if tokenErr != nil {
-				t.Fatal(tokenErr)
+func TestTokenAuthenticator(t *testing.T) {
+	tests := []struct {
+		name                string
+		audiences           authenticator.Audiences
+		accesstoken         *oauthv1.OAuthAccessToken
+		user                *userv1.User
+		reviewedToken       *string
+		expectedResponse    *authenticator.Response
+		expectedError       string
+		expectAuthenticated bool
+	}{
+		{
+			name:      "no authorization header",
+			audiences: authenticator.Audiences{"someaud"},
+		},
+		{
+			name:          "empty token",
+			reviewedToken: sptr(""),
+			audiences:     authenticator.Audiences{"someaud"},
+		},
+		{
+			name: "non-existent user",
+			accesstoken: &oauthv1.OAuthAccessToken{
+				ObjectMeta: metav1.ObjectMeta{Name: "atokencreatedmanuallybymetyping"},
+				UserName:   "pepa",
+				UserUID:    "some-uid",
+			},
+			user: &userv1.User{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "tonda",
+					UID:  "tonda-uid",
+				},
+			},
+			reviewedToken: sptr("atokencreatedmanuallybymetyping"),
+			expectedError: `users.user.openshift.io "pepa" not found`,
+		},
+		{
+			name: "non-existent token",
+			accesstoken: &oauthv1.OAuthAccessToken{
+				ObjectMeta: metav1.ObjectMeta{Name: "adifferenttokenfromtheoneintherequest"},
+				UserName:   "jenda",
+				UserUID:    "some-uid",
+				Scopes:     []string{"user:check-access"},
+			},
+			user: &userv1.User{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "jenda",
+					UID:  "some-uid",
+				},
+			},
+			reviewedToken: sptr("sometokenthatiscertainlynotpresent"),
+			expectedError: "token lookup failed",
+		},
+		{
+			name: "invalid user UID in the token",
+			accesstoken: &oauthv1.OAuthAccessToken{
+				ObjectMeta: metav1.ObjectMeta{Name: "atokencreatedmanuallybymetyping"},
+				UserName:   "usertypek",
+				UserUID:    "weird-uid",
+				Scopes:     []string{"user:full"},
+			},
+			user: &userv1.User{
+				ObjectMeta: metav1.ObjectMeta{Name: "usertypek"},
+			},
+			reviewedToken: sptr("atokencreatedmanuallybymetyping"),
+			audiences:     authenticator.Audiences{"someaud"},
+			expectedError: "user.UID () does not match token.userUID (weird-uid)",
+		},
+		{
+			name: "valid token review",
+			accesstoken: &oauthv1.OAuthAccessToken{
+				ObjectMeta: metav1.ObjectMeta{Name: "atokencreatedmanuallybymetyping"},
+				UserName:   "usertypek",
+				UserUID:    "some-uid",
+				Scopes:     []string{"user:full"},
+			},
+			user: &userv1.User{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "usertypek",
+					UID:  "some-uid",
+				},
+			},
+			reviewedToken: sptr("atokencreatedmanuallybymetyping"),
+			expectedResponse: &authenticator.Response{
+				User: &user.DefaultInfo{
+					Name:   "usertypek",
+					UID:    "some-uid",
+					Groups: []string{"system:authenticated:oauth"},
+					Extra: map[string][]string{
+						"scopes.authorization.openshift.io": {"user:full"},
+					},
+				},
+			},
+			expectAuthenticated: true,
+		},
+		{
+			name: "valid token review with audiences",
+			accesstoken: &oauthv1.OAuthAccessToken{
+				ObjectMeta: metav1.ObjectMeta{Name: "atokencreatedmanuallybymetyping"},
+				UserName:   "usertypek",
+				UserUID:    "some-uid",
+				Scopes:     []string{"user:full"},
+			},
+			user: &userv1.User{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "usertypek",
+					UID:  "some-uid",
+				},
+			},
+			reviewedToken: sptr("atokencreatedmanuallybymetyping"),
+			audiences:     authenticator.Audiences{"someaud", "someotheraud"},
+			expectedResponse: &authenticator.Response{
+				Audiences: authenticator.Audiences{"someaud", "someotheraud"},
+				User: &user.DefaultInfo{
+					Name:   "usertypek",
+					UID:    "some-uid",
+					Groups: []string{"system:authenticated:oauth"},
+					Extra: map[string][]string{
+						"scopes.authorization.openshift.io": {"user:full"},
+					},
+				},
+			},
+			expectAuthenticated: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest("POST", "https://localhost", nil)
+			if tt.reviewedToken != nil {
+				req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *tt.reviewedToken))
 			}
-			t.Errorf("Found token (created=%s, timeout=%di, now=%s), but it should be gone!",
-				token.CreationTimestamp, token.InactivityTimeoutSeconds, current.Now())
-		}
-		if err != errTimedout {
-			t.Errorf("Unexpected error checking absence of token %s: %v", name, err)
-		}
-		if userInfo != nil {
-			t.Errorf("Unexpected user checking absence of token %s: %v", name, userInfo)
-		}
+			if tt.audiences != nil {
+				req = req.WithContext(authenticator.WithAudiences(req.Context(), tt.audiences))
+			}
+
+			users := []runtime.Object{}
+			if tt.user != nil {
+				users = append(users, tt.user)
+			}
+			tokens := []runtime.Object{}
+			if tt.accesstoken != nil {
+				tokens = append(tokens, tt.accesstoken)
+			}
+
+			userClient := userfake.NewSimpleClientset(users...)
+			userInformer := userinformer.NewSharedInformerFactory(userClient, time.Second*60)
+			if err := userInformer.User().V1().Groups().Informer().AddIndexers(cache.Indexers{
+				usercache.ByUserIndexName: usercache.ByUserIndexKeys,
+			}); err != nil {
+				t.Fatalf("failed to create user index: %v", err)
+			}
+
+			h := TokenAuthenticator{
+				accessTokenClient: oauthfake.NewSimpleClientset(tokens...).OauthV1().OAuthAccessTokens(),
+				userClient:        userClient.UserV1().Users(),
+				groupMapper:       usercache.NewGroupCache(userInformer.User().V1().Groups()),
+				// testing just a single validator to prove errors propagate
+				validators: tokenvalidators.OAuthTokenValidators{tokenvalidators.NewUIDValidator()},
+			}
+			gotResponse, authenticated, err := h.AuthenticateRequest(req)
+
+			if err != nil {
+				if len(tt.expectedError) == 0 {
+					t.Errorf("expected no error, got %v", err)
+				} else if tt.expectedError != err.Error() {
+					t.Errorf("expected error %q, got %q", tt.expectedError, err.Error())
+				}
+			}
+
+			if len(tt.expectedError) > 0 && err == nil {
+				t.Errorf("expected error %q, but error is nil", tt.expectedError)
+			}
+
+			if tt.expectAuthenticated != authenticated {
+				t.Errorf("expected authenticated: %v, got %v", tt.expectAuthenticated, authenticated)
+			}
+
+			if !equality.Semantic.DeepEqual(tt.expectedResponse, gotResponse) {
+				t.Errorf("expected != got: %s", diff.ObjectDiff(tt.expectedResponse, gotResponse))
+			}
+		})
 	}
 }
 
+func sptr(s string) *string {
+	return &s
 }
